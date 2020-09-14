12350d721760e0ddfca95506f7261151cea200e2
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-09-14 21:24:30 +0200

    --- Auto-Git Commit ---

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):   # db table
+    title = models.CharField(max_length=50)  # column
+    content = models.TextField(max_length=2000)
+
+
+    def __str__(self):
+        return self.title
