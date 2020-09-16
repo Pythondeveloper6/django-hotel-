7cf532a59768730e3d2ed1c41b2d93ffc312b1b1
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-09-16 21:17:10 +0200

    --- Auto-Git Commit ---

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+POST_TYPE = (
+    ('DRAFT','DRAFT'),
+    ('PUBLISHED' , 'PUBLISHED'),
+)
+
 
 class Post(models.Model):   # db table
-    title = models.CharField(max_length=50)  # column
-    content = models.TextField(max_length=2000)
+    title = models.CharField(max_length=50 , unique=True)  # column
+    content = models.TextField(max_length=2000 , null=True , blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=False)
+    author_mail = models.EmailField(default='')
+    type = models.CharField(choices=POST_TYPE , default='DRAFT',max_length=20)
+    image = models.ImageField(upload_to='posts/')
+
 
 
     def __str__(self):
         return self.title
+
+    # def __unicode__(self):
+    #     return self.title
