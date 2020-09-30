6f38b7d675badb5cb57e7a4afb86fc96a4fb7ac0
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-09-30 20:44:49 +0200

    --- Auto-Git Commit ---

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, related_name='user_profile', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/')
+    phone_number = models.CharField(max_length=16)
+    address = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return str(self.user)
